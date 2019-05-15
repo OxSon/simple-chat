c9d3e442ffd7f15f7d71af9c982acecbf143c0a6
OxSon <theoxson@gmail.com>
2019-05-15 01:35:01 -0600

    Initial commit

--- chatserver/chat/models.py
+++ chatserver/chat/models.py
@@ -1,3 +1,18 @@
+from django.conf import settings
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Channel(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+class Message(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    text = models.TextField()
+    timestamp = models.DateTimeField(default=timezone.now)
+    channel = models.ForeignKey('Channel', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.author}, {self.channel.name}: {self.text} @ {self.timestamp}"
--- chatserver/chat/views.py
+++ chatserver/chat/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import JsonResponse, Http404
+from django.forms.models import model_to_dict
 
-# Create your views here.
+from .models import Channel, Message
+
+def channels(request):
+    if request.method == 'GET':
+        channels = list(map(lambda x : model_to_dict(x), Channel.objects.all()))
+        return JsonResponse(channels, safe=False)
+    else if request.method == 'POST':
+        channel = Channel(name=request.POST['name'])
+        channel.save()
+        return JsonResponse({"pk": channel.pk})
+    else
+        return Http404("Invalid request type")
+
+
+def channel_detail(request, pk):
+    channel = get_object_or_404(Channel, pk=pk)
+    return JsonResponse(model_to_dict(channel))
+
+def messages(request, pk):
+    if request.method == 'GET':
+        channel = get_object_or_404(Channel, pk=pk)
+        messages = list(map(lambda m : model_to_dict(m), \
+                Message.objects.filter(channel=channel).order_by(timestamp)))
+
+        return JsonResponse(messages, safe=False)
+    else if request.method == 'POST':
+        text = request.POST['text'];
+        message = Message(text=text, channel=pk)
+        message.save()
+        return JsonResponse({"pk": message.pk})
+    else
+        return Http404('Invalid request type')

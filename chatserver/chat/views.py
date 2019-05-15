588807c18dbe886f09e7b5f3c8b1eb37b9ac8fc9
OxSon <theoxson@gmail.com>
2019-05-15 01:38:04 -0600

    Add colons where needed

--- chatserver/chat/views.py
+++ chatserver/chat/views.py
@@ -8,11 +8,11 @@ def channels(request):
     if request.method == 'GET':
         channels = list(map(lambda x : model_to_dict(x), Channel.objects.all()))
         return JsonResponse(channels, safe=False)
-    else if request.method == 'POST':
+    elif request.method == 'POST':
         channel = Channel(name=request.POST['name'])
         channel.save()
         return JsonResponse({"pk": channel.pk})
-    else
+    else:
         return Http404("Invalid request type")
 
 
@@ -27,10 +27,10 @@ def messages(request, pk):
                 Message.objects.filter(channel=channel).order_by(timestamp)))
 
         return JsonResponse(messages, safe=False)
-    else if request.method == 'POST':
+    elif request.method == 'POST':
         text = request.POST['text'];
         message = Message(text=text, channel=pk)
         message.save()
         return JsonResponse({"pk": message.pk})
-    else
+    else:
         return Http404('Invalid request type')

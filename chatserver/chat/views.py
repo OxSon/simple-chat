6fcd4e132970ab98a5c0cdda116a4707181431f5
OxSon <theoxson@gmail.com>
2019-05-17 00:10:21 -0600

    Refactor Http responses

--- chatserver/chat/views.py
+++ chatserver/chat/views.py
@@ -1,55 +1,55 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import JsonResponse, Http404, HttpResponse
 from django.forms.models import model_to_dict
-#FIXME
-from django.views.decorators.csrf import csrf_exempt, csrf_protect
 
 from .models import Channel, Message
 
-#FIXME
-@csrf_exempt
 def channels(request):
     if request.method == 'GET':
         channels = list(map(lambda x : model_to_dict(x), Channel.objects.all()))
-        return JsonResponse(channels, safe=False)
-    elif request.method == 'POST':
-        name = request.POST.get('name')
+        return JsonResponse({"status": True, "Channels": channels}, safe=False)
+    elif request.method == 'PUT':
+        name = request.headers['name']
 
-        if name == '':
-            response = HttpResponse("'name' cannot be the empty string")
-            response.status_code = 422
-            return response
+        if not name:
+            return JsonResponse({"status": False,
+                "message": "must include 'name' header"}, status=405)
         elif Channel.objects.filter(name=name).count() > 0:
-            response = HttpResponse(f"channel with name {name} already exists")
-            response.status_code = 422
-            return response
+            return JsonResponse({"status": True}, status=201)
         else:
             channel = Channel(name=name)
             channel.save()
-            return JsonResponse({"pk": channel.pk})
+            return JsonResponse({"pk": channel.pk}, status=200)
     else:
-        return Http404("Invalid request type")
-
+        return bad_request_type
 
 def channel_detail(request, pk):
-    channel = get_object_or_404(Channel, pk=pk)
-    return JsonResponse(model_to_dict(channel))
+    if request.method == 'GET':
+        channel = get_object_or_404(Channel, pk=pk)
+        return JsonResponse({"status": True, "Channel": model_to_dict(channel)},
+                status=200)
+    #FIXME NotImplemented
+    elif request.method == 'DELETE':
+        raise NotImplementedError
+    else:
+        return bad_request_type
 
-#FIXME
-@csrf_exempt
 def messages(request, pk):
     if request.method == 'GET':
-        #FIXME Does the below need to be casted?
-        #channel = Channel(get_object_or_404(Channel, pk=pk))
         channel = get_object_or_404(Channel, pk=pk)
         messages = list(map(lambda m : model_to_dict(m), \
                 Message.objects.filter(channel=channel).order_by('timestamp')))
 
-        return JsonResponse(messages, safe=False)
+        return JsonResponse(messages, safe=False, status=200)
     elif request.method == 'POST':
         text = request.POST['text']
         message = Message(text=text, channel=Channel.objects.get(pk=pk), author=request.user)
         message.save()
-        return JsonResponse({"pk": message.pk})
+        return JsonResponse({"pk": message.pk}, status=200)
     else:
-        return Http404('Invalid request type')
+        return bad_request_type
+
+def bad_request_type(request):
+        return JsonResponse({"status": False,
+                "message": f"request method {request.method} not supported"},
+                status=405)
